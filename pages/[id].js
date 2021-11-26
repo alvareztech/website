712b90b10f2f8d36505d8d9c182b8ec774f36df4
Daniel Alvarez <daniel.alvarez@dexcom.com>
2021-11-25 19:48:48 -0800

    Chacka UI added

--- pages/[id].js
+++ pages/[id].js
@@ -2,7 +2,6 @@ import Layout from '../components/layout'
 import { getAllPostIds, getPostData } from '../lib/posts'
 import Head from 'next/head'
 import Date from '../components/date'
-import utilStyles from '../styles/utils.module.css'
 
 export default function Post({ postData }) {
   return (
@@ -11,8 +10,8 @@ export default function Post({ postData }) {
         <title>{postData.title}</title>
       </Head>
       <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
+        <h1>{postData.title}</h1>
+        <div>
           <Date dateString={postData.date} />
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
