84aada234c300b254b2272a100f8e95d3ab90513
Daniel Alvarez <daniel.alvarez@dexcom.com>
2021-11-28 21:14:05 -0800

    Tailwinds article styles

--- pages/[id].js
+++ pages/[id].js
@@ -3,18 +3,46 @@ import { getAllPostIds, getPostData } from '../lib/posts'
 import Head from 'next/head'
 import Date from '../components/date'
 
+const editUrl = (slug) =>
+  `https://github.com/alvareztech/website/edit/main/data/blog/${slug}.md`;
+
 export default function Post({ postData }) {
   return (
     <Layout>
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <article className="mx-auto w-10/12">
+      <article className="flex flex-col justify-center items-start max-w-3xl mx-auto mb-16 w-full">
         <h1>{postData.title}</h1>
-        <div>
+        <div className="text-base text-gray-500 mb-2">
           <Date dateString={postData.date} />
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <div className="flex items-start justify-center mt-14 w-full">
+          <div className="space-x-2 flex-1 -mt-0.5">
+            {/* {frontMatter.tags?.map(tag => ( */}
+            <span
+              key="hello"
+              className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200">
+              hello
+            </span>
+            <span
+              key="hello"
+              className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200">
+              hello
+            </span>
+            {/* ))} */}
+          </div>
+          <div className="ml-4 text-sm text-gray-500 dark:text-gray-500 transition-colors hover:text-gray-700 dark:hover:text-gray-300">
+            <a
+              href={editUrl(postData.id)}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {'Edit on Github'}
+            </a>
+          </div>
+        </div>
       </article>
     </Layout>
   )
