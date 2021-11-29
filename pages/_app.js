804168f7bbf2bfb68fdee9316038786cd3464fe8
Daniel Alvarez <daniel.alvarez@dexcom.com>
2021-11-28 16:56:50 -0800

    ChackaUI removed

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
-// import '../styles/global.css'
-import { ChakraProvider } from "@chakra-ui/react"
+import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Component {...pageProps} />
   )
 }
